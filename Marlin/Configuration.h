268395bddc5d83a3a325d224f0271c615ea2af2d
Daniel Berg <mail@roosta.sh>
2018-11-25 17:30:52 +0100

    Fix LCD

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1662,7 +1662,7 @@
 // RepRapDiscount FULL GRAPHIC Smart Controller
 // http://reprap.org/wiki/RepRapDiscount_Full_Graphic_Smart_Controller
 //
-#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
+// #define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
 
 //
 // ReprapWorld Graphical LCD
@@ -1711,7 +1711,7 @@
 //
 // LCD for Melzi Card with Graphical LCD
 //
-//#define LCD_FOR_MELZI
+#define LCD_FOR_MELZI
 
 //
 // SSD1306 OLED full graphics generic display
